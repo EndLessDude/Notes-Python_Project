2d6621c2b73d1e5431f7edc1b42784e56f129156
EndLessDude <universetrickster@gmail.com>
2026-07-23 17:12:46 -0400

    Added ability to add and delete notes

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from os.path import dirname, abspath
+from flask_login import LoginManager
+
+# Database
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dudududumaxverstapen'
+    app.config['SECRET_KEY'] = 'dudududumaxverstapen' # just a random string of letters
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.join(dirname(abspath(__file__)), DB_NAME)}' # tells where to create the database
+    db.init_app(app) #tells flask that we are using this database
 
     from .views import views
     from .auth import auth  
@@ -10,4 +22,22 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app): #checks if database exists, if not then it creates one
+    if not path.exists(path.join(dirname(abspath(__file__)), DB_NAME)):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
---
+++ Website/models.py
@@ -0,0 +1,17 @@
+from . import db #imports the innitial db object
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) #func basically just gets the corrent date and time according to the timezone
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #stores an ID which will be used in the database
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
